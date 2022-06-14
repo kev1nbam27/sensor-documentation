e4589cee046d3d24a8af5a730234af7056ec8be4
Kevin Bam <kevin1bam@web.de>
2022-06-14 14:39:24 +0200

    Englisch added, auto-language, auto-mode

--- js/main.js
+++ js/main.js
@@ -1,6 +1,27 @@
+var known = { en: true, de: true};
+var lang = 'en';
+
 $(document).ready(function() {
 
     SyntaxHighlighter.defaults['toolbar'] = false;
     SyntaxHighlighter.all();
+    
+    if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+        document.querySelector('html').classList.toggle('dark', true);
+    }
+    
+    lang  = ((navigator.languages && navigator.languages[0]) || navigator.language || navigator.userLanguage || 'en').substr(0, 2);
 
+    if(!known[lang])
+        lang = 'en';
+
+    $('div[lang='  + lang + ']').show();
+
+    if (lang == "en") {
+        $('div[lang=' + "de" + ']').hide();
+        document.querySelector('html').classList.toggle('en', false);
+    } else if (lang == "de") {
+        $('div[lang=' + "en" + ']').hide();
+        document.querySelector('html').classList.toggle('en', true);
+    }
 });
